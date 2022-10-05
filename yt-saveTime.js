a369759b6135e932ad1656f4056acdd71303054e
Tony Wu <tonyz840101@gmail.com>
2022-10-05 11:20:38 +0800

    Update yt-saveTime.js

--- yt-saveTime.js
+++ yt-saveTime.js
@@ -1,1 +1,1 @@
-javascript:window.location.replace(window.location.href+'&t='+document.getElementsByClassName("ytp-time-current")[0].innerHTML.split(':').reduce((p,c) => p*60 + parseInt(c),0));
+javascript:window.location.replace(window.location.href+'&t='+document.getElementsByClassName("ytp-time-current")[0].innerHTML.split(':').reduce((p,c) => p*60 + parseInt(c),0)+'s');
